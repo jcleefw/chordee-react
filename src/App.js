e8d3457d0df762b5931d3533454029e317b7dfc3
JC Lee <jcleefw@gmail.com>
2019-09-25 01:41:33 +1000

    spike routing with hookrouter:)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 import './App.scss';
+
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+import {useRoutes} from 'hookrouter';
+
+const routes = {
+  '/': () => <HomePage />,
+  '/about': () => <AboutPage />,
+  // '/products': () => <ProductOverview />,
+  // '/products/:id': ({id}) => <ProductDetails id={id} />
+};
+
+const App = () => {
+  const routeResult = useRoutes(routes);
+
+  return routeResult || <NotFoundPage />;
 }
+
 
 export default App;
