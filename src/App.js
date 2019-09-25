b1efab9b8d31cf848ecf6e829f0a0c04870bd662
JC Lee <jcleefw@hotmail.com>
2019-09-25 17:05:37 +1000

    introduction of react-bulma-components into code base. setup all base required file to compile

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-
-import {useRoutes} from 'hookrouter';
+import { useRoutes } from 'hookrouter';
 
 const routes = {
   '/': () => <HomePage />,
