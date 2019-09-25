d99809ee0ac7078660d525298bd5676f778fb3e4
JC Lee <jcleefw@hotmail.com>
2019-09-25 17:46:59 +1000

    add navbar to chordee

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,13 @@ const Header = () => {
   return <Navbar>
     <Navbar.Brand>
       <Navbar.Item renderAs="a" href="#">
-        <img
-          src="https://bulma.io/images/bulma-logo.png"
-          alt="Bulma: a modern CSS framework based on Flexbox"
-          width="112"
-          height="28"
-        />
+        Chordee
       </Navbar.Item>
     </Navbar.Brand>
     <Navbar.Menu>
       <Navbar.Container>
-        <Navbar.Item href="#">Second</Navbar.Item>
-      </Navbar.Container>
-      <Navbar.Container position="end">
-        <Navbar.Item href="#">At the end</Navbar.Item>
+        <Navbar.Item href="/">Home</Navbar.Item>
+        <Navbar.Item href="/about">About</Navbar.Item>
       </Navbar.Container>
     </Navbar.Menu>
   </Navbar>
