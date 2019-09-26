c7146cba122c7f274d0149f5d5afcf579c6ffe10
JC Lee <jcleefw@hotmail.com>
2019-09-26 10:23:08 +1000

    add songFormatter for chords and lyrics. Havent handle tags yet

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import Header from '../components/Header';
-import ChordSheetJS from 'chordsheetjs';
+import HtmlSongDisplayer from '../components/HtmlSongDiplayer';
 
-function viewChords() {
-  const chordSheet = `
-      Am         C/G        F          C
-    Let it be, let it be, let it be, let it be
-    C                G              F  C/E Dm C
-    Whisper words of wisdom, let it be`.substring(1);
-
-  const parser = new ChordSheetJS.ChordSheetParser();
-  const song = parser.parse(chordSheet);
-  const formatter = new ChordSheetJS.HtmlTableFormatter();
-  console.log(formatter)
-  const disp = formatter.format(song);
-  return disp
-}
+const chordSheet = `
+Am         C/G        F          C
+Let it be, let it be, let it be, let it be
+C                G              F  C/E Dm C
+Whisper words of wisdom, let it be`.substring(1)
 
 const HomePage = () => {
   return <div>
     <Header />
-    {viewChords()}
+    <section>
+      <div className="columns">
+        <div className="column">
+          {chordSheet}
+        </div>
+        <div className="column">
+          <HtmlSongDisplayer chordSheet={chordSheet} />
+        </div>
+      </div>
+    </section>
+
   </div>
 }
 
