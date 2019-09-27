94f256eedc1d6921cc7e6718e7f4a3a53798ed5a
JC Lee <jcleefw@hotmail.com>
2019-09-27 22:31:48 +1000

    update api index file to connect to mongo db  for CRUD action in web

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,104 @@
-import axios from 'axios'
+function ChordeeApi() {
+  const BASE_URL = 'http://localhost:3000';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
-})
+  const getConfig = {
+    method: 'GET',
 
-export const insertSong = payload => api.post(`/song`, payload)
-export const getAllSongs = () => api.get(`/songs`)
-export const updateSongById = (id, payload) => api.put(`/song/${id}`, payload)
-export const deleteSongById = id => api.delete(`/song/${id}`)
-export const getSongById = id => api.get(`/song/${id}`)
+    headers: {
+      Accept: 'application/json',
+    },
+  };
 
-const apis = {
-  insertSong,
-  getAllSongs,
-  updateSongById,
-  deleteSongById,
-  getSongById,
+  const postConfig = {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const putConfig = {
+    method: 'put',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const deleteConfig = {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  /**
+   * The response.json() is a promise that reads the HTTP response
+   * body, from the network input stream.
+   *
+   * We resolve the Promise when the body is completely read, and
+   * along with the body data, we pass a few fields from the original
+   * response to the client code.
+   *
+   *
+   * @param  {object} response The Response object as defined in the Fetch Api
+   *
+   * @return {object}          The parsed JSON body, and a number of fields
+   *                           from the response
+   */
+  const parseJson = response => new Promise(
+    resolve => response.json().then(json => resolve({
+      status: response.status,
+      ok: response.ok,
+      json,
+    })),
+  );
+
+  const makeRequest = (url, options) => (
+    new Promise(
+      (resolve, reject) => fetch(url, options)
+        .then(parseJson)
+        .then(
+          response => (response.ok ? resolve(response.json) : reject(response.json)),
+        ),
+    )
+  );
+
+  const makeGetRequest = url => makeRequest(url, getConfig);
+  const makePostRequest = (url, body = {}) => makeRequest(
+    url,
+    Object.assign({}, postConfig, { body: JSON.stringify(body) }),
+  );
+  const makePutRequest = (url, body = {}) => makeRequest(
+    url,
+    Object.assign({}, putConfig, { body: JSON.stringify(body) }),
+  );
+  const makeDeleteRequest = url => makeRequest(url, deleteConfig);
+
+  return {
+    fetchAllSong() {
+      const url = BASE_URL + '/api/songs/';
+      return makeGetRequest(url);
+    },
+
+    createSong(data) {
+      const url = '/api/song/';
+      return makePostRequest(url, data);
+    },
+
+    fetchSong(id) {
+      const url = `/api/song/${id}`;
+      return makeGetRequest(url);
+    },
+
+    updateSong(id, data) {
+      const url = `/api/song/${id}`;
+      return makePutRequest(url, data);
+    },
+
+    deleteSong(id) {
+      const url = `/api/delete-song/${id}`
+      return makeDeleteRequest(url);
+    },
+  };
 }
 
-export default apis
+export default ChordeeApi;
