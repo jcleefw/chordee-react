668411e38c38b670872dbaf5d0ae220ad601b176
JC Lee <jcleefw@hotmail.com>
2019-09-27 22:32:55 +1000

    create Songlist page with Table

---
+++ src/pages/SongsList.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import ChordeeApi from '../api';
+import Header from '../components/Header';
+
+function SongsList() {
+  const chordeeApi = new ChordeeApi();
+  const [songs, setSongs] = useState([]);
+
+  function fetchSongs() {
+    chordeeApi.fetchAllSong().then((data) => {
+      setSongs(data.songs);
+    });
+  }
+  useEffect(fetchSongs, []);
+
+  const displaySongs = () => {
+    if (songs.length > 0) {
+      return songs.map(song => (
+        displaySongRow(song)
+      ))
+    }
+  }
+
+  const displaySongRow = (song) => (
+    <tr key={song.name}>
+      <td>{song.name}</td>
+      <td>{song.artist.join()}</td>
+      <td>{song.format}</td>
+    </tr>
+  )
+
+
+  return <div>
+    <Header />
+    <section>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Artist</th>
+            <th>Format</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displaySongs()}
+        </tbody>
+      </table>
+    </section>
+  </div>
+}
+
+export default SongsList;
